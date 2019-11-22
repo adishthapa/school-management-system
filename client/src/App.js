0ab8fc7c2ebd92d09563d5e615400882f65823c7
adishthapa <adishthapa@outlook.com>
2019-11-21 20:35:54 -0500

    Added Auth0 to the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
+import { Router, Route, Switch } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import PrivateRoute from "./components/PrivateRoute";
+import Profile from "./components/Profile";
+import { useAuth0 } from "./react-auth0-spa";
+import history from "./utils/history";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World!</h1>
+        <Router history={history}>
+          <header>
+            <NavBar />
+          </header>
+          <Switch>
+            <Route path="/" exact />
+            <PrivateRoute path="/profile" component={Profile} />
+          </Switch>
+        </Router>
       </div>
     );
   }
