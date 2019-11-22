82c8d007991b9ed19058ba8342c6a4e68f8799a5
adishthapa <adishthapa@outlook.com>
2019-11-21 20:05:37 -0500

    Added skeleton code for the app

---
+++ models/user.js
@@ -0,0 +1,13 @@
+// User model
+// ============================================================================
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
